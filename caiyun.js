d191b7174ecd621c7706b8f86bde4e28b7ebfbbe
yanyuwangluo <70591645+yanyuwangluo@users.noreply.github.com>
2023-10-31 20:38:24 +0800

    1
    1

---
+++ caiyun.js
@@ -0,0 +1,89 @@
+/*
+ * @Author: yanyuwangluo 1915241107@qq.com
+ * @Date: 2023-10-31 20:31:04
+ * @LastEditors: yanyuwangluo 1915241107@qq.com
+ * @LastEditTime: 2023-10-31 20:33:33
+ * @FilePath: \青龙shell\caiyun.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/*************************************
+
+项目名称：彩云天气-净化/SVIP
+脚本作者：佚名
+使用声明：⚠️仅供参考，🈲售卖！
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/(biz|wrapper)\.cyapi\.cn\/(.+\/(user.+|visitors|activity)|p\/v\d\/vip_info) url script-response-body https://raw.githubusercontent.com/Boughtlike/quantumultx/main/rewrite/caiyun.js
+^http:\/\/adx\.sogaha\.cn\/sdk\/ad\/get url reject-200
+
+[mitm]
+hostname = *.cyapi.cn, adx.sogaha.cn
+
+*************************************/
+
+
+var chxm1023 = JSON.parse($response.body);
+const urls = ['/vip_info', '/activity', 'user'];
+
+urls.forEach(url => {
+  if ($request.url.includes(url)){
+    switch(url) {
+      case '/vip_info':
+        chxm1023 = {
+          ...chxm1023, 
+          vip: {
+            "expires_time" : "4092599349",
+            "is_auto_renewal" : true
+          },
+          svip: {
+            "expires_time" : "4092599349",
+            "is_auto_renewal" : true
+          }
+        }
+        break;
+      case '/activity':
+        chxm1023.activities = [];
+        break;
+      case 'user':
+        chxm1023.result = {
+          ...chxm1023.result,
+          ranking_above: 99,
+          is_vip: true,
+          vip_expired_at: 4092599349,
+          svip_given: 9999,
+          is_xy_vip: true,
+          xy_svip_expire: 4092599349,
+          wt: {
+            ...chxm1023.result.wt,
+            vip: {
+              "auto_renewal_type" : "",
+              "expired_at" : 0,
+              "enabled" : true,
+              "svip_apple_expired_at" : 4092599349,
+              "is_auto_renewal" : true,
+              "svip_expired_at" : 4092599349,
+              "svip_auto_renewal_type" : ""
+            },
+            svip_given: 9999,
+            ranking_above: 99,
+          },
+          is_phone_verified: true,
+          phone_num: "13145200000",
+          vip_take_effect: 1,
+          is_primary: true,
+          xy_vip_expire: 4092599349,
+          svip_expired_at: 4092599349,
+          svip_take_effect: 1,
+          vip_type: "s",
+          name: "烟雨",
+          avatar: "https://tu.yanyuwangluo.cn/2021/12/21/fe0632cfd5b47.png",
+          token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps"
+        }
+        break;
+    }
+  }
+})
+
+$done({ body: JSON.stringify(chxm1023)});
